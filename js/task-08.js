52da479fc33c0937b0d35cef80e6df7bb5f2ad25
Ihor-M28 <114598607+Ihor-M28@users.noreply.github.com>
2023-03-26 20:21:18 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,33 +1,33 @@
-// const formRef = document.querySelector(".login-form");
-// formRef.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     const {
-//         elements: { login, password }
-//     } = event.currentTarget;
-
-//     if (login.value === "" || password.value === "") {
-//         return alert("всі поля повинні бути заповнені!");
-//     }
-//     console.log(`Login: ${login.value}, Password: ${password.value}`);
-//     event.currentTarget.reset();
-// }
-
-const form = document.querySelector(".login-form");
-form.addEventListener("submit", handleSubmit);
+const formRef = document.querySelector(".login-form");
+formRef.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault();
-    const login = event.currentTarget.elements.email;
-    const password = event.currentTarget.elements.password;
+    event.preventDefault();
+    const {
+        elements: { password, login }
+    } = event.currentTarget;
 
-  if (login.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
-
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
+    if (password.value === "" || login.value === "") {
+        return alert("всі поля повинні бути заповнені!");
+    }
+    console.log(`Login: ${password.value}, Password: ${login.value}`);
+    event.currentTarget.reset();
 }
+
+// const form = document.querySelector(".login-form");
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//     const login = event.currentTarget.elements.email;
+//     const password = event.currentTarget.elements.password;
+
+//   if (login.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
 
 
