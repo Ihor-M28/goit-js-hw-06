7039d53c4737926d650872f871bf4daf11971c50
Ihor-M28 <114598607+Ihor-M28@users.noreply.github.com>
2023-03-27 16:15:15 +0300

    new
    new

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,21 @@
+
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+    const login = event.currentTarget.elements.email;
+    const password = event.currentTarget.elements.password;
+
+  if (login.value === "" || password.value === "") {
+    return console.log("всі поля повинні бути заповнені!");
+  }
+
+  console.log(`{Login: ${login.value}, Password: ${password.value}}`);
+  event.currentTarget.reset();
+}
+
+
 // const formRef = document.querySelector(".login-form");
 // formRef.addEventListener("submit", handleSubmit);
 
@@ -13,21 +31,3 @@
 //     console.log(`Login: ${password.value}, Password: ${password.value}`);
 //     event.currentTarget.reset();
 // }
-
-const form = document.querySelector(".login-form");
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-    const login = event.currentTarget.elements.email;
-    const password = event.currentTarget.elements.password;
-
-  if (login.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
-
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
-
-
